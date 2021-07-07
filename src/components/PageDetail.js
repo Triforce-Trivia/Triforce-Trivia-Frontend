c574e2a51e03841f5d8e5f3f0194e97450aa3107
Elise Muellerleile <eliamue@live.com>
2021-07-07 12:21:32 -0500

    signin functionality

---
+++ src/components/PageDetail.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+// import { rightAnswer, wrongAnswer } from '../zombie/Z1.js'
+
+
+class PageDetail extends Component {
+
+    state = {
+        bgImage: "url(../../zombies/z0.gif)",
+        backgroundSize: "cover",
+        height: "600px",
+        backgroundRepeat: "no-repeat",
+    };
+
+    render() {
+        console.log(this.props)
+        const {question} = this.props
+        return (
+        <div>
+            <p 
+                style={{
+                backgroundImage: this.state.bgImage,
+                backgroundSize: "cover",
+                height: "600px",
+                backgroundRepeat: "no-repeat",
+                overflow: "visible"
+                }}
+                >
+
+                    <h3>{this.props.question}</h3>
+
+                    <button onClick={this.props.rightAnswer}>
+                        {this.props.correct_answer}
+                    </button>
+
+                    <button onClick={this.wrongAnswer}>
+                        {this.props.incorrect_answer}
+                    </button>
+                
+            </p>
+
+        </div>
+        )
+    }
+}
+export default PageDetail
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Auth0ProviderWithHistroy from './auth0-provider-with-history'
-import {BrowserRouter as Router} from 'react-router-dom'
+
 ReactDOM.render(
-  <Router>
-    <Auth0ProviderWithHistroy > 
+  <React.StrictMode>
+    
     <App /> 
-    </Auth0ProviderWithHistroy> 
-  </Router>,
+
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
