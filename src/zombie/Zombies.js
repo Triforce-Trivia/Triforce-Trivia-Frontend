c93093be2bec38d6fcd6554d66abf4e20b543c97
Elise Muellerleile <eliamue@live.com>
2021-07-07 19:22:13 -0500

    gif routes now external, fetch utils file created, findbyid started

--- src/zombie/Zombies.js
+++ src/zombie/Zombies.js
@@ -1,47 +1,76 @@
 const zombies = [
+    
     {
         id: 1,
-        url: "https://i.imgur.com/jIsHfiA.gifv"
+        url: "https://i.imgur.com/0n6VBH0.gifv"
     },
     {
         id: 2,
-        url: "https://i.imgur.com/w9jnrBd.gifv"
+        url: "https://i.imgur.com/KJGReKc.gifv"
     },
     {
         id: 3,
-        url: "https://i.imgur.com/cPgYWbQ.gifv"
+        url: "https://i.imgur.com/"
     },
     {
         id: 4,
-        url: "https://i.imgur.com/87hJ05I.gifv"
+        url: "https://i.imgur.com/"
     },
     {
         id: 5,
-        url: "https://i.imgur.com/8Ftb7zx.gif"
+        url: "https://i.imgur.com/VNtxHH0.gifv"
     },
     {
         id: 6,
-        url: "https://i.imgur.com/ioQU4kl.gifv"
+        url: "https://i.imgur.com/"
     },
     {
         id: 7,
-        url: "https://i.imgur.com/SC4kNjL.gifv"
+        url: "https://i.imgur.com/"
     },
     {
         id: 8,
+        url: "https://i.imgur.com/"
+    },
+    {
+        id: 0,
+        url: "https://i.imgur.com/pqfHf2s.gifv"
+    },
+    {
+        id: -1,
+        url: "https://i.imgur.com/jIsHfiA.gifv"
+    },
+    {
+        id: -2,
+        url: "https://i.imgur.com/cPgYWbQ.gifv"
+    },
+    {
+        id: -3,
+        url: "https://i.imgur.com/87hJ05I.gifv"
+    },
+    {
+        id: -4,
+        url: "https://i.imgur.com/8Ftb7zx.gif"
+    },
+    {
+        id: -5,
+        url: "https://i.imgur.com/ioQU4kl.gifv"
+    },
+    {
+        id: -6,
+        url: "https://i.imgur.com/SC4kNjL.gifv"
+    },
+    {
+        id: -7,
         url: "https://i.imgur.com/ESgo5ve.gifv"
     },
     {
-        id: 9,
+        id: -8,
         url: "https://i.imgur.com/swmnBWq.gifv"
     },
     {
-        id: 10,
+        id: -10,
         url: "https://i.imgur.com/0ljkW42.gifv"
-    },
-    {
-        id: 10,
-        url: "./z10.jpg"
     }
     ];
     
