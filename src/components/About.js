273174ab6fce866ecea2e49b62effdc53d1df296
Elise Muellerleile <eliamue@live.com>
2021-07-08 12:46:53 -0500

    about page image and names added

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,24 @@ import '../style/About.css'
 export default class AboutUs extends Component {
     render() {
         return (
-            <div>
-                About Us:
+            <div
+            className="bk_Img"
+            style={{
+            backgroundImage: "url(../../about/mystery-machine.png)",
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            height: "400px",
+            }}>
+                <h2>Meet the Gang!</h2>
+                
+                    <h4 className="dimitra">Dimitra</h4>
+                
+                    <h4 className="chen">Chen</h4>
+                
+                    <h4 className="elise">Elise</h4>
 
-                We are awesome
+                    <h4 className="scooby">Scooby</h4>
+
             </div>
         )
     }
