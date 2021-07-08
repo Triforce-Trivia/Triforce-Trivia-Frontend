a6e66a9d204e8e1c92013befd69243fe9994b2a7
Elise Muellerleile <eliamue@live.com>
2021-07-07 21:07:44 -0500

    werewolf gifs and array added

---
+++ src/werewolf/Werewolves.js
@@ -0,0 +1,74 @@
+const werewolves = [
+    {
+        id: 8,
+        url: "https://i.imgur.com/"
+    },
+    {
+        id: 7,
+        url: "https://i.imgur.com/"
+    },
+    {
+        id: 6,
+        url: "https://i.imgur.com/"
+    },
+    {
+        id: 5,
+        url: "https://i.imgur.com/"
+    },
+    {
+        id: 4,
+        url: "https://i.imgur.com/"
+    },
+    {
+        id: 3,
+        url: "https://i.imgur.com/"
+    },
+    {
+        id: 2,
+        url: "https://i.imgur.com/"
+    },
+    {
+        id: 1,
+        url: "https://i.imgur.com/"
+    },
+
+    {
+        id: 0,
+        url: "https://i.imgur.com/KDBaTxn.gifv"
+    },
+    
+    {
+        id: -1,
+        url: "https://i.imgur.com/aTs66Jy.gifv"
+    },
+    {
+        id: -2,
+        url: "https://i.imgur.com/7Cm8Viu.gifv"
+    },
+    {
+        id: -3,
+        url: "https://i.imgur.com/3cFCT3v.gifv"
+    },
+    {
+        id: -4,
+        url: "https://i.imgur.com/YvA2GLQ.gifv"
+    },
+    {
+        id: -5,
+        url: "https://i.imgur.com/20tU6Oy.gifv"
+    },
+    {
+        id: -6,
+        url: "https://i.imgur.com/p43vTbG.gifv"
+    },
+    {
+        id: -7,
+        url: "https://i.imgur.com/LFR1gvt.gifv"
+    },
+    {
+        id: -8,
+        url: "https://i.imgur.com/3tPA6R5.gifv"
+    }
+    ];
+    
+    export default werewolves
