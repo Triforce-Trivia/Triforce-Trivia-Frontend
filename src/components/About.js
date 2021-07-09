9695c49ba609110f1e32fa3f6a7e669aa78ba778
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-07-09 12:44:27 -0700

    fixed eslint issues

--- src/components/About.js
+++ src/components/About.js
@@ -19,7 +19,7 @@ export default class AboutUs extends Component {
                         <a><h4 className="dimitra">
                             Dimitra
                         </h4></a>
-                        <h3 className="hide"  className="dimitra-bio">
+                        <h3 className="dimitra-bio">
                             Dimitra is a Fullstack Software Engineer and is interested in creating apps and supporting companies, startups, etc. that have a mission-driven mindset to effect positive change in their local communities and abroad.
                             She is passionate about developing and providing solutions to everyday problems, and finding ways to give access to resources for those who are marginalized, overlooked, and discounted. It is her dream to use her coding experience to help people achieve their goals.
                         </h3>
@@ -29,7 +29,7 @@ export default class AboutUs extends Component {
                         <h4 className="chen">
                             Chen
                         </h4>
-                        <h3 className="hide" className="chen-bio">
+                        <h3 className="chen-bio">
                             CHen is a Fullstack Software Engineer and is a curious person who likes to learn new things and challenge myself.  I like to travel to different countries and meet a people with diverse background. When I am not coding, I usually spend time jogging or meditating.
                         </h3>
                     </section>
@@ -38,7 +38,7 @@ export default class AboutUs extends Component {
                         <h4 className="elise">
                             Elise
                         </h4>
-                        <h3 className="hide" className="elise-bio">
+                        <h3 className="elise-bio">
                             Elise is a Fullstack Software Engineer, dancer, gamer, cat mom, and costumer with a love for CSS.
                         </h3>
                     </section>
@@ -47,7 +47,7 @@ export default class AboutUs extends Component {
                         <h4 className="scooby">
                             Scooby
                         </h4>
-                        <h3 className="hide"  className="scooby-bio">
+                        <h3 className="scooby-bio">
                             Scooby is a Very Good Boi.
                         </h3>
                     </section>
