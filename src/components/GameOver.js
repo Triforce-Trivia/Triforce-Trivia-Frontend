9d3b55ce48bf1c947b53401b4a16487566f4c8d7
Elise Muellerleile <eliamue@live.com>
2021-07-09 12:53:19 -0500

    sad shaggy is sad when you die

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../style/GameOver.css'
 
 export default class GameOver extends Component {
     render() {
         return (
-            <div>
-                Game Over
-                GIF
+            <div className="gameover">
+                <h3>Mr. Jenkins ran off, and whatever has been happening in these woods may never be solved. </h3>
+                <p className="end">Game Over, man, game over!</p>
+                <img className="shaggy-gif"src="https://media1.tenor.com/images/62ca7d092e9d04f24056cecabba3282f/tenor.gif?itemid=5665424" alt="sad shaggy"></img>
             </div>
         )
     }
